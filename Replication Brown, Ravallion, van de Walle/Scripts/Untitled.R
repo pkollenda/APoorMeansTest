607f9dcb560d80834f4c901c4e383cd26168de0b
Philipp Kollenda <pkollenda@gmail.com>
2021-06-04 17:24:42 +0200

    Delete multiple files

--- Replication Brown, Ravallion, van de Walle/Scripts/Untitled.R
+++
@@ -1,7 +0,0 @@
-mymean <- function(data, var) {
-  data <- inner_join(data, households[c("HHID", "mult")], by = "HHID")
-  svy <- svydesign(ids = ~1, weights = ~mult, data = data)
-  
-  means <- svymean(formula(paste("~", var)), design = svy) %>% round(digits = 3)
-  print(means)
-}
